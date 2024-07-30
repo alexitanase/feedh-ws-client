d38d934e0df14a7e4537d0ca5d7676bf96ca7f1c
Iulian Alexandru Tanase <iuliantanase@feedh.net>
2024-07-30 13:06:12 +0200

    Remove API service endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 module.exports = {
   websocket_server: "https://websocket.feedh.net",
-  service_endpoint: "https://api-v2.feedh.net/i/_PARTNER_/tracker",
   partner_code: "",
   socket_options: {
     reconnection: true,
